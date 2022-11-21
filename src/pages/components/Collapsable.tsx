13b24b2ac9385d95572754b8b731a841847f35ad
greypeg <gpapas028@gmail.com>
2022-11-21 13:41:11 +0000

    starting second form and adding validation

--- src/pages/components/Collapsable.tsx
+++ src/pages/components/Collapsable.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
 
 const Collapsable = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <div>
       <div className="container mr-44 ml-44 py-16 px-12">
@@ -24,57 +33,67 @@ const Collapsable = () => {
             </div>
           </summary>
           <div className="p-4">
-            <form>
-                <div  className="grid grid-cols-2">
-
-              
-              <div className="md:w-1/3">
-              <label
-                  className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
-                  for="inline-full-name"
-                >
-                  First Name
-                </label>
-                <input
-                  className="w-fit appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
-                  id="inline-full-name"
-                  type="text"
-                  value="Jane Doe"
-                />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid grid-cols-2">
+                <div className="md:w-1/3">
+                  <label
+                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    for="inline-full-name"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    {...register("firstname", { required: true })}
+                    className="w-fit appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    id="inline-full-name"
+                    type="text"
+                  />
+                  {errors.firstname && (
+                    <span className="text-red-500">This field is required</span>
+                  )}
+                </div>
+                <div className="md:w-2/3">
+                  <label
+                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    for="surname-name"
+                  >
+                    Surname
+                  </label>
+                  <input
+                    {...register("lastname", { required: true })}
+                    className="w-fit appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    id="surname-name"
+                    type="text"
+                  />
+                  <br></br>
+                  {errors.lastname && (
+                    <span className="text-red-500">This field is required</span>
+                  )}
+                </div>
+                <div className="md:w-3/3">
+                  <label
+                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    for="email-address"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    {...register("email", { required: true })}
+                    className="w-fit appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    id="email-address"
+                    type="email"
+                  />
+                  <br></br>
+                  {errors.email && (
+                    <span className="text-red-500">This field is required</span>
+                  )}
+                </div>
+                <div>
+                  <button type="submit" className="bg-indigo-700 text-white">
+                    Next
+                  </button>
+                </div>
               </div>
-              <div className="md:w-2/3">
-              <label
-                  className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
-                  for="surname-name"
-                >
-                  Surname
-                </label>
-                <input
-                  className="w-fit appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
-                  id="surname-name"
-                  type="text"
-                  value="Jane Doe"
-                />
-              </div>
-              <div className="md:w-3/3">
-              <label
-                  className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
-                  for="email-address"
-                >
-                 Email Address
-                </label>
-                <input
-                  className="w-fit appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
-                  id="email-address"
-                  type="text"
-                  value="Jane Doe"
-                />
-              </div>
-              <div>
-              <button className="bg-indigo-700 text-white">Next</button>
-              </div>
-              </div>
-              
             </form>
           </div>
         </details>
@@ -97,13 +116,41 @@ const Collapsable = () => {
             </div>
           </summary>
           <div className="p-4">
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
-              praesentium officia nostrum iste eaque, explicabo sunt, voluptatum
-              mollitia corporis porro repudiandae. Eaque neque voluptatum sint
-              molestiae? Assumenda eligendi facilis reprehenderit.
-              <button className="bg-indigo-700 text-white">Next</button>
-            </p>
+            <form>
+              <div className="grid grid-cols-2">
+                <div className="md:w-1/3">
+                  <label
+                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    for="telephone"
+                  >
+                    Telephone Number
+                  </label>
+                  <input
+                    {...register("telephone", { required: true })}
+                    className="w-fit appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    id="telephone"
+                    type="tel"
+                  />
+                  <br></br>
+                  {errors.email && (
+                    <span className="text-red-500">This field is required</span>
+                  )}
+                </div>
+                <div className="md:w-2/3">
+                <label
+                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    for="gender"
+                  >
+                    Gender
+                  </label>
+                  <select {...register("gender")}>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="other">other</option>
+                  </select>
+                </div>
+              </div>
+            </form>
           </div>
         </details>
 
