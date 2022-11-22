1481a7448d9892dd771bd02e11d3f6917a5b44e1
greypeg <gpapas028@gmail.com>
2022-11-22 09:36:14 +0000

    made fixes to the UI

--- src/pages/components/Collapsable.tsx
+++ src/pages/components/Collapsable.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useForm, SubmitHandler  } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { useState } from "react";
 import { trpc } from "../../utils/trpc";
 
 type FormValues = {
-    firstname: string;
-    surname: string;
-    email: string;
-    dateOfBirth: string;
-    gender: string;
-    tel: string;
-    comments : string;
-  };
+  firstname: string;
+  surname: string;
+  email: string;
+  birthday: string;
+  gender: string;
+  telephone: string;
+  comments: string;
+};
 const Collapsable = () => {
-
   const mutation = trpc.example.submitData.useMutation();
   const {
     register,
@@ -24,25 +23,14 @@ const Collapsable = () => {
   } = useForm<FormValues>();
 
   const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => {
-    mutation.mutate({name: data.firstname, surname: data.surname, email: data.email});
+    mutation.mutate({
+      name: data.firstname,
+      surname: data.surname,
+      email: data.email,
+    });
     console.log(data);
   };
 
-  const {
-    register: register2,
-    formState: { errors: errors2 },
-    handleSubmit: handleSubmit2,
-  } = useForm({
-    mode: "onBlur",
-  });
-
-  const {
-    register: register3,
-    formState: { errors: errors3 },
-    handleSubmit: handleSubmit3,
-  } = useForm({
-    mode: "onBlur",
-  });
   const [section, setSection] = useState(0);
 
   const isOpen = (sectionNumber: number) => {
@@ -50,18 +38,17 @@ const Collapsable = () => {
     return false;
   };
 
-
   return (
     <div>
       <div className="container rounded-lg bg-white py-4 px-4 shadow-xl">
         <details
-          className="group mb-2 rounded bg-white bg-[#DEDEDE] shadow"
+          className="group mb-2 rounded-xl rounded bg-white bg-[#DEDEDE] shadow"
           open={isOpen(0)}
           id="0"
         >
           <summary
             className="relative flex cursor-pointer list-none flex-wrap
-    items-center rounded focus-visible:outline-none
+    items-center rounded focus-visible:outline-none 
     focus-visible:ring focus-visible:ring-pink-500 group-open:z-[1] group-open:rounded-b-none
     "
             onClick={(e) => {
@@ -70,7 +57,7 @@ const Collapsable = () => {
               else setSection(0);
             }}
           >
-            <h3 className="flex flex-1 bg-yellow-400 p-4 font-semibold text-white">
+            <h3 className="flex flex-1 rounded-lg bg-yellow-400 p-4 font-semibold text-white">
               Step 1: Your details
             </h3>
           </summary>
@@ -81,13 +68,13 @@ const Collapsable = () => {
               <div className="grid grid-cols-2 gap-0">
                 <div className="md:w-1/">
                   <label
-                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    className="mb-1 block pr-4 font-bold text-black md:mb-0"
                     htmlFor="inline-full-name"
                   >
                     First Name
                   </label>
                   <input
-                    className="w-fit appearance-none rounded-lg rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    className="w-fit appearance-none rounded-xl rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
                     id="inline-full-name"
                     type="text"
                     {...register("firstname", { required: true })}
@@ -99,31 +86,31 @@ const Collapsable = () => {
                 </div>
                 <div className="md:w-2/3">
                   <label
-                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    className="mb-1 block pr-4 font-bold text-black md:mb-0"
                     htmlFor="surname-name"
                   >
                     Surname
                   </label>
                   <input
-                    className="w-fit appearance-none rounded-lg rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    className="w-fit appearance-none rounded-xl rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
                     id="surname-name"
                     type="text"
                     {...register("surname", { required: true })}
                   />
                   <br></br>
-                  {errors.lastname && (
+                  {errors.surname && (
                     <span className="text-red-500">This field is required</span>
                   )}
                 </div>
                 <div className="md:w-3/3">
                   <label
-                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    className="mb-1 block pr-4 font-bold text-black md:mb-0"
                     htmlFor="email-address"
                   >
                     Email Address:
                   </label>
                   <input
-                    className="w-fit appearance-none rounded-lg rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    className="w-fit appearance-none rounded-xl rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
                     id="email-address"
                     type="email"
                     {...register("email", { required: true })}
@@ -140,7 +127,7 @@ const Collapsable = () => {
                 <div className="col-span-1 col-end-7">
                   <button
                     type="submit"
-                    className="h-8 w-36 rounded-lg bg-indigo-700 text-white"
+                    className="h-8 w-36 rounded-xl bg-indigo-700 text-white"
                   >
                     Next {" >"}
                   </button>
@@ -150,7 +137,7 @@ const Collapsable = () => {
           </div>
         </details>
         <details
-          className="group mb-2 rounded bg-white bg-[#DEDEDE] shadow"
+          className="group mb-2 rounded-xl rounded bg-white bg-[#DEDEDE] shadow"
           open={isOpen(1)}
           id="1"
         >
@@ -165,40 +152,45 @@ const Collapsable = () => {
               else setSection(1);
             }}
           >
-            <h3 className="flex flex-1 bg-yellow-400 p-4 font-semibold  text-white">
+            <h3 className="flex flex-1 rounded-lg bg-yellow-400 p-4 font-semibold  text-white">
               Step 2: More comments
             </h3>
           </summary>
           <div className="p-4">
-            <form onSubmit={handleSubmit2(onSubmit)}>
+            <form>
               <div className="grid grid-cols-2">
                 <div className="md:w-1/3">
                   <label
-                   className="mb-1 w-64 block pr-4 font-bold text-gray-500 md:mb-0"
+                    className="mb-1 block w-64 pr-4 font-bold text-black md:mb-0"
                     htmlFor="telephone"
                   >
                     Telephone Number
                   </label>
                   <input
-                    className="w-fit appearance-none rounded-lg rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                    className="w-fit appearance-none rounded-xl rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:outline-none"
                     id="telephone"
                     type="tel"
-                    {...register2("telephone", { required: true })}
+                    {...register("telephone", { required: true })}
                   />
                   <br></br>
-                  {errors2.email && (
+                  {errors.email && (
                     <span className="text-red-500">This field is required</span>
                   )}
                 </div>
                 <div className="md:w-2/3">
                   <label
-                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    className="mb-1 block pr-4 font-bold text-black md:mb-0"
                     htmlFor="gender"
                   >
                     Gender
                   </label>
-                  <select {...register2("gender")}>
-                  <option value="" disabled selected hidden>Select Gender</option>
+                  <select
+                    {...register("gender")}
+                    className="w-fite rounded-xl rounded border-2 border-b-4 border-gray-200 border-gray-500 bg-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+                  >
+                    <option value="" disabled selected hidden>
+                      Select Gender
+                    </option>
                     <option value="female">female</option>
                     <option value="male">male</option>
                     <option value="other">other</option>
@@ -206,16 +198,74 @@ const Collapsable = () => {
                 </div>
                 <div className="md:w-3/3">
                   <label
-                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    className="mb-1 block pr-4 font-bold text-black md:mb-0"
                     htmlFor="birthday"
                   >
                     Date of birth
                   </label>
+                  <select
+                    id="month"
+                    name="month"
+                    className="w-12 appearance-none focus:w-24 rounded-xl rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:outline-none"
+                  >
+                    <option value="" selected disabled hidden></option>
+                    <option value="01">January</option>
+                    <option value="02">February</option>
+                    <option value="03">March</option>
+                    <option value="04">April</option>
+                    <option value="05">May</option>
+                    <option value="06">June</option>
+                    <option value="07">July</option>
+                    <option value="08">August</option>
+                    <option value="09">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                  </select>
+                  <select
+                    id="day"
+                    name="day"
+                    className="mx-2 w-12 focus:w-24 appearance-none rounded-xl rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:outline-none"
+                  >
+                    <option value="" selected disabled hidden></option>
+                    <option value="01">1</option>
+                    <option value="02">2</option>
+                    <option value="03">3</option>
+                    <option value="04">4</option>
+                    <option value="05">5</option>
+                    <option value="06">6</option>
+                    <option value="07">7</option>
+                    <option value="08">8</option>
+                    <option value="09">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+                  </select>
                   <input
-                    type="date"
-                    id="birthday"
-                    className="rounded-lg"
-                    {...register2("birthday", { required: true })}
+                    className="w-12 appearance-none focus:w-24 rounded-xl rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:outline-none"
+                    type="number"
+                    min="1900"
+                    max="2099"
+                    step="1"
                   />
                 </div>
                 <div></div>
@@ -224,7 +274,7 @@ const Collapsable = () => {
                 <div className="col-span-1 col-end-7">
                   <button
                     type="submit"
-                    className="h-8 w-36 rounded-lg bg-indigo-700 text-white"
+                    className="h-8 w-36 rounded-xl bg-indigo-700 text-white"
                   >
                     Next {" >"}
                   </button>
@@ -235,7 +285,7 @@ const Collapsable = () => {
         </details>
 
         <details
-          className="group mb-2 rounded bg-white bg-[#DEDEDE] shadow"
+          className="group mb-2 rounded-xl rounded bg-white bg-[#DEDEDE] shadow"
           open={isOpen(2)}
           id="2"
         >
@@ -250,17 +300,16 @@ const Collapsable = () => {
               else setSection(2);
             }}
           >
-            <h3 className="flex flex-1 bg-yellow-400 p-4 font-semibold  text-white">
+            <h3 className="flex flex-1 rounded-xl bg-yellow-400 p-4 font-semibold  text-white">
               Step 3: Final comments
             </h3>
           </summary>
           <div className="p-4">
-            <form onSubmit={handleSubmit3(onSubmit)}>
+            <form>
               <div className="grid grid-cols-2">
-                
                 <div className="">
                   <label
-                    className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0"
+                    className="mb-1 ml-1 block pr-4 font-bold text-black md:mb-1"
                     htmlFor="comments"
                   >
                     Comments
@@ -269,10 +318,11 @@ const Collapsable = () => {
                     id="comments"
                     rows={4}
                     cols={50}
-                    {...register3("comments", { required: true })}
+                    className="rounded-xl"
+                    {...register("comments", { required: true })}
                   ></textarea>
                 </div>
-                <div className="col-end-7 col-span-2 row-end-3">
+                <div className="col-span-2 col-end-7 row-end-3">
                   <button
                     type="submit"
                     className="h-8 w-36 rounded-lg bg-indigo-700 text-white"
